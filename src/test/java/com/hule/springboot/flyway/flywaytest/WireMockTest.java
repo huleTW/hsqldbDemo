0f3599b97a981945608fe7d4a641955d9d506f32
Hu Le <le.hu.2010@163.com>
2019-12-17 15:56:48 +0800

    add wirmock test using rule

--- src/test/java/com/hule/springboot/flyway/flywaytest/WireMockTest.java
+++ src/test/java/com/hule/springboot/flyway/flywaytest/WireMockTest.java
@@ -1,4 +1,44 @@
 package com.hule.springboot.flyway.flywaytest;
 
-public class WiremockTest {
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class WireMockTest {
+
+    @Autowired
+    private UserClient userClient;
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options().port(9009));
+
+    @Test
+    public void statusMessage() {
+        stubFor(get(urlEqualTo("/"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody("{\"id\":\"id\", \"firstName\":\"hu\", \"lastName\":\"le\", \"career\":\"Engineer\"}")
+                        .withHeader("Content-Type", "application/json")));
+
+        User user = userClient.getUser("id");
+        assertEquals(user.getId(), "id");
+        assertEquals(user.getFirstName(), "hu");
+        assertEquals(user.getLastName(), "le");
+        assertEquals(user.getCareer(), "Engineer");
+
+    }
 }
